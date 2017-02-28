ed2e221c62003d450f278bfef2080992048735ef
Shaka Huang <shakalaca@gmail.com>
2017-02-28 10:18:29 +0800

    Pass parameters to ndk-build
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- 03_build_fastboot.sh
+++ 03_build_fastboot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ndk-build NDK_PROJECT_PATH=. NDK_APP_LIBS_OUT=dist NDK_APPLICATION_MK=Application_fastboot.mk
+ndk-build NDK_PROJECT_PATH=. NDK_APP_LIBS_OUT=dist NDK_APPLICATION_MK=Application_fastboot.mk $@
